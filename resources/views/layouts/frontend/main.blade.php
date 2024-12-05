8250c2c9bc1aa97851edacefa3fd30ea275633e1
reishantridyarafly <reishantridyarafly@gmail.com>
2024-12-05 07:45:06 +0700

    Feat meta: Update meta site

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -5,6 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="NGP Interior Design">
+    <meta property="og:description" content="Professional Interior Design Services">
+    <meta property="og:image" content="{{ asset('backend/assets') }}/images/logo_ngp_2.png">
+    <meta property="og:url" content="http://ngpinteriorkuningan.servicesolutions.my.id/">
+    <meta property="og:site_name" content="NGP Interior Design">
     <!-- Title -->
     <title>@yield('title') || {{ config('app.name') }}</title>
     <!-- Favicon -->
